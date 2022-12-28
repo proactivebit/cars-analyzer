03dd5f8c1de143cd9149b03ff6b7b1b583699135
Dawid <Kurek>
2022-12-28 15:56:46 +0100

    add paging

--- server/carsService.js
+++ server/carsService.js
@@ -1,14 +1,30 @@
 import axios from "axios";
 
-const url =
-  "https://api.cepik.gov.pl/pojazdy?wojewodztwo=12&data-od=20220101&data-do=20221101&typ-daty=2&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&limit=100&page=1";
+const formatUrl = (province, startDate, endDate, page) =>
+  `https://api.cepik.gov.pl/pojazdy?wojewodztwo=${province}&data-od=${startDate}&data-do=${endDate}&typ-daty=1&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&limit=500&page=${page}`;
 
 class CarsService {
   async getCars(province, startDate, endDate) {
-    const response = await axios.get(
-      `https://api.cepik.gov.pl/pojazdy?wojewodztwo=${province}&data-od=${startDate}&data-do=${endDate}&typ-daty=1&tylko-zarejestrowane=true&pokaz-wszystkie-pola=false&limit=500&page=1`
-    );
-    return response.data;
+    let response = {
+      data: [],
+      meta: {
+        count: 0,
+      },
+    };
+    let page = 1;
+    do {
+      const path = formatUrl(province, startDate, endDate, page);
+      console.log(`start fetch data ${path}`);
+      const result = await axios.get(path);
+      if (!Array.isArray(result.data.data)) {
+        break;
+      }
+      response.data = [...response.data, ...result.data.data];
+      response.meta = result.data.meta;
+      console.log(`end fetch data ${response.data.length}`);
+      page++;
+    } while (response.data.length < response.meta.count);
+    return response;
   }
 }
 
